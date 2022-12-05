0cf502b921d2899a4cba8d636e3f8632ec194645
Camila1548 <luistrotta@gmail.com>
2022-12-05 18:25:35 -0300

    Terminación de módulo de listComprehensions

--- Curso_Python-master/06. programacion-funcional/list_comprehensions/listComprehensionsAnidadas.py
+++ Curso_Python-master/06. programacion-funcional/list_comprehensions/listComprehensionsAnidadas.py
@@ -4,16 +4,21 @@ lista_listas = [[1,2,3],[4,5,6],[7,8,9]]
 lista_simple = [valor for sublista in lista_listas for valor in sublista]
 print(f'lista simple: {lista_simple}')
 
+
+
+
 # Ahora creamos una lista de numeros pares a partir de la lista_listas
 # Sin list comprehensions, ciclos for anidados
 lista_pares = []
-for sublista in lista_listas:
-    for valor in sublista:
+for sublist in lista_listas:
+    for valor in sublist:
         if valor%2==0:
             lista_pares.append(valor)
 print(f'Lista pares: {lista_pares}')
 
-# Con list comprehensions, en una sola línea de código
-# No es necesario separar las líneas, solo es para mejor lectura de código
+
+
+#Con list comprehensions, en una sola línea de código
+#No es necesario separar las líneas, solo es para mejor lectura de código
 lista_pares = [valor for sublista in lista_listas for valor in sublista if valor%2==0]
 print(f'Lista pares: {lista_pares}')
